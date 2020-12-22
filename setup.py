6411e64886e2ef3b625dfd9d2e54a62a7f10d495
zachjweiner <zachjweiner@gmail.com>
2020-12-22 11:01:39 -0600

    udpates to setup.py

--- setup.py
+++ setup.py
@@ -1,36 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
+from pathlib import Path
 from setuptools import setup, find_packages, Command
 
 PACKAGE_NAME = "pystella"
 
 
-# authoritative version in pytools/__init__.py
 def find_git_revision(tree_root):
-    # Keep this routine self-contained so that it can be copy-pasted into
-    # setup.py.
+    tree_root = Path(tree_root).resolve()
 
-    from os.path import join, exists, abspath
-    tree_root = abspath(tree_root)
-
-    if not exists(join(tree_root, ".git")):
+    if not tree_root.joinpath(".git").exists():
         return None
 
-    from subprocess import Popen, PIPE, STDOUT
-    p = Popen(["git", "rev-parse", "HEAD"], shell=False,
-              stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True,
-              cwd=tree_root)
-    (git_rev, _) = p.communicate()
+    from subprocess import run, PIPE, STDOUT
+    result = run(["git", "rev-parse", "HEAD"], shell=False,
+                 stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True,
+                 cwd=tree_root)
 
+    git_rev = result.stdout
     git_rev = git_rev.decode()
-
     git_rev = git_rev.rstrip()
 
-    retcode = p.returncode
-    assert retcode is not None
-    if retcode != 0:
+    assert result.returncode is not None
+    if result.returncode != 0:
         from warnings import warn
         warn("unable to find git revision")
         return None
@@ -39,12 +32,10 @@ def find_git_revision(tree_root):
 
 
 def write_git_revision(package_name):
-    from os.path import dirname, join
-    dn = dirname(__file__)
+    dn = Path(__file__).parent
     git_rev = find_git_revision(dn)
-
-    with open(join(dn, package_name, "_git_rev.py"), "w") as outf:
-        outf.write('GIT_REVISION = "%s"\n' % git_rev)
+    text = 'GIT_REVISION = "%s"\n' % git_rev
+    dn.joinpath(package_name, "_git_rev.py").write_text(text)
 
 
 write_git_revision(PACKAGE_NAME)
@@ -58,14 +49,15 @@ class PylintCommand(Command):
     ]
 
     def initialize_options(self):
-        if os.path.exists("setup.cfg"):
-            self.pylint_rcfile = "setup.cfg"
+        setup_cfg = Path("setup.cfg")
+        if setup_cfg.exists():
+            self.pylint_rcfile = setup_cfg
         else:
             self.pylint_rcfile = None
 
     def finalize_options(self):
         if self.pylint_rcfile:
-            assert os.path.exists(self.pylint_rcfile)
+            assert Path(self.pylint_rcfile).exists()
 
     def run(self):
         command = ["pylint"]
